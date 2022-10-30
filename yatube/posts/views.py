34645d27f8ea52a6849352eb426c9d2860714022
vartex <vartexxx29@yandex.ru>
2022-10-30 16:47:31 +0500

    feature: создана и добавлена в админку модель Post

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
     template = 'posts/index.html'
     title = 'Главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
+        'posts': posts,
     }
     return render(request, template, context)
 
